917e13b7210fc73d0b2b6f41e428f492b3678f09
gft102 <gft102@localiza.com>
2022-12-13 11:34:57 -0300

    change to module way

--- k3s-terraform/main.tf
+++ k3s-terraform/main.tf
@@ -1,57 +1,24 @@
-resource "oci_identity_compartment" "_" {
-  name          = var.name
-  description   = var.name
-  enable_delete = true
-}
-
-locals {
-  compartment_id = oci_identity_compartment._.id
-}
-
-data "oci_identity_availability_domains" "_" {
-  compartment_id = local.compartment_id
-}
-
-data "oci_core_images" "_" {
-  compartment_id           = local.compartment_id
+module "oci_core_instance" {
+  source = "git::https://github.com/alessonviana/OCI_MODULES.git"
+  #instance_count             = 1 # how many instances do you want?
+  #ad_number                  = 1 # AD number to provision instances. If null, instances are provisionned in a rolling manner starting with AD1
+  #compartment_ocid           = var.compartment_ocid
+  #source_ocid                = var.source_ocid
+  #subnet_ocids               = var.subnet_ocids
+  #public_ip                  = var.public_ip # NONE, RESERVED or EPHEMERAL
+  #ssh_public_keys            = var.ssh_public_keys
+  #block_storage_sizes_in_gbs = [50]
+  how_many_nodes           = var.how_many_nodes
   shape                    = var.shape
-  operating_system         = "Canonical Ubuntu"
-  operating_system_version = "20.04"
-  #operating_system         = "Oracle Linux"
-  #operating_system_version = "7.9"
+  operating_system         = var.operating_system
+  operating_system_version = var.operating_system_version
+  memory_in_gbs_per_node   = var.memory_in_gbs_per_node
+  ocpus_per_node           = var.ocpus_per_node
+  #instance_state             = var.instance_state # RUNNING or STOPPED
+  #boot_volume_backup_policy  = var.boot_volume_backup_policy # disabled, gold, silver or bronze
 }
 
-resource "oci_core_instance" "_" {
-  for_each            = local.nodes
-  display_name        = each.value.node_name
-  availability_domain = data.oci_identity_availability_domains._.availability_domains[var.availability_domain].name
-  compartment_id      = local.compartment_id
-  shape               = var.shape
-  shape_config {
-    memory_in_gbs = var.memory_in_gbs_per_node
-    ocpus         = var.ocpus_per_node
-  }
-  source_details {
-    source_id   = data.oci_core_images._.images[0].id
-    source_type = "image"
-  }
-  create_vnic_details {
-    subnet_id  = oci_core_subnet._.id
-    private_ip = each.value.ip_address
-  }
-  metadata = {
-    ssh_authorized_keys = join("\n", local.authorized_keys)
-  }
+module "oci_core_vcn" {
+  source     = "git::https://github.com/alessonviana/OCI_MODULES.git"
+  cidr_block = var.cidr_block
 }
-
-locals {
-  nodes = {
-    for i in range(1, 1 + var.how_many_nodes) :
-    i => {
-      node_name  = format("node%d", i)
-      ip_address = format("192.168.24.%d", 192 + i)
-      role       = i == 1 ? "controlplane" : "worker"
-    }
-  }
-}
-
