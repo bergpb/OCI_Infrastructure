d22a170ffa0255d72da575024d08889d82bd16e8
gft102 <gft102@localiza.com>
2023-01-13 22:56:36 -0300

    rename compartment

--- k3s-terraform/main.tf
+++ k3s-terraform/main.tf
@@ -1,6 +1,6 @@
 module "k3s" {
   source                   = "git::https://github.com/alessonviana/OCI_MODULES.git"
-  name                     = "K3S_CLUSTER2" #compartment name
+  name                     = "K3S_CLUSTER" #compartment name
   memory_in_gbs_per_node   = "6" 
   shape                    = "VM.Standard.A1.Flex"
 }
