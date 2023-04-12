ce63772b5028226ac7a313efc3b15cd84f5cee7c
gft102 <gft102@localiza.com>
2023-04-12 08:06:41 -0300

    Remove some trash in the code

--- k3s-terraform/backend.tf
+++ k3s-terraform/backend.tf
@@ -4,12 +4,3 @@ terraform {
     update_method = "PUT"
   }
 }
-#terraform {
-#  backend "s3" {
-#    bucket         = "terraform_state.tfstate"
-#    key            = "https://objectstorage.us-ashburn-1.oraclecloud.com/n/ids4dyhxibcj/b/terraform-states/o/terraform_state.tfstate"
-#    region         = "us-ashburn-1"
-#    profile        = "<nome_do_perfil_oci>"
-#    shared_credentials_file = "/Users/${USER}/.oci"
-#  }
-#}
