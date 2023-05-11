e9e974974f1f9f174177fb8e3a0da202c224cb3f
gft102 <gft102@localiza.com>
2023-05-11 10:20:04 -0300

    disable Traefik with default

--- k3s-terraform/backend.tf
+++ k3s-terraform/backend.tf
@@ -1,6 +1,7 @@
-terraform {
-  backend "http" {
-    address       = "https://objectstorage.us-ashburn-1.oraclecloud.com/n/ids4dyhxibcj/b/terraform-states/o/terraform_state.tfstate"
-    update_method = "PUT"
-  }
-}
+#terraform {
+#  backend "http" {
+#    address       = "https://objectstorage.us-ashburn-1.oraclecloud.com/n/ids4dyhxibcj/b/terraform-states/o/terraform_state.tfstate"
+#    encrypt        = true
+#    update_method = "PUT"
+#  }
+#}
